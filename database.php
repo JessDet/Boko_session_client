3a7add3d2ee8d84a0bdde25bfea796e419877273
boulanger <olivier.dww@gmail.com>
2022-01-14 23:21:22 +0100

    fix pb 14-01-2022

--- database.php
+++ database.php
@@ -1,11 +1,11 @@
 <?php
 
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=projet_boko', 'root', '', [
+    $pdo = new PDO('mysql:host=localhost;dbname=boko', 'root', '', [
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]);
 } catch (PDOException $e) {
     echo $e->getMessage();
- }
- 
- return $pdo;
+}
+
+return $pdo;
--- profile.php
+++ profile.php
@@ -5,7 +5,7 @@ require './isLoggedIn.php';
 $user = isLoggedIn();
 
 if (!$user) {
-    header ('Location: /connexion.php');
+    header('Location: /connexion.php');
 }
 
 
@@ -17,6 +17,7 @@ $pdo = require './database.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -25,30 +26,29 @@ $pdo = require './database.php';
     <link rel="stylesheet" href="/CSS/Header-Footer.css">
     <title>Connexion</title>
 </head>
-<body>
-<?php require_once'includes/header.php' ?>
 
-<nav class="log">     
-    <a href="/logout.php/"><img class="logout_icon" src="icons/logout_90894.png" alt="Se déconnecter" ></a>       
-</nav>
+<body>
+    <?php require_once 'includes/header.php' ?>
+
+    <nav class="log">
+        <a href="/logout.php/"><img class="logout_icon" src="icons/logout_90894.png" alt="Se déconnecter"></a>
+    </nav>
 
     <h1>PROFIL</h1>
     <h2>Hello <?= $user['pseudo'] ?></h2>
 
 
     <div class="info_profil">
-    <?php foreach ($utilisateur as $a) : ?>
-            <span><?= $a['firstname'] ?></span>
-            <span><?= $a['lastname'] ?></span>
-            <span><?= $a['adress'] ?></span>
-            <span><?= $a['phone'] ?></span>
-            <span><?= $a['email'] ?></span>
-            
-    <?php endforeach; ?>
-</div>
+        <span><?= $user['firstname'] ?></span>
+        <span><?= $user['lastname'] ?></span>
+        <span><?= $user['adress'] ?></span>
+        <span><?= $user['phone'] ?></span>
+        <span><?= $user['email'] ?></span>
+    </div>
 
 
-    
-<?php require_once'includes/footer.php' ?>
+
+    <?php require_once 'includes/footer.php' ?>
 </body>
+
 </html>
